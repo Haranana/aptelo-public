3111372c6fbe8a1aa764f07458aeae0b977c80ac
Haranana <hubert.osmalek@op.pl>
2025-09-22 10:37:27 +0200

    Revert "test update"
    This reverts commit 3b59a6df9fdbea8e4acb09da9f58cd7c427dc9cb.

--- szablony/standardowy.rwd.v2/css/style.php
+++ szablony/standardowy.rwd.v2/css/style.php
@@ -143,7 +143,6 @@ function SzablonZapiszCacheCss($NazwaPlikuCache, $CssDoZaladowania, $tpl) {
         'programy/slickSlider/slick-theme.css',
         'programy/jBox/jBox.all.css',
     ];
-    //sd/
     foreach ($ext as $p) {
         if (file_exists($p)) {
             $parts[] = "/* === $p === */\n" . file_get_contents($p);
