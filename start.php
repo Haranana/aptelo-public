e5b0128fa0f46cad993f16ee8f83b05005796f1a
Haranana <hubert.osmalek@op.pl>
2025-09-22 19:59:02 +0200

    test update

--- start.php
+++ start.php
@@ -304,7 +304,7 @@ if ( SZEROKOSC_SKLEPU != '' ) {
 // moduly srodkowe nad czescia glowna sklepu z boxami   
 //$tpl->dodaj('__MODULY_SRODKOWE_GORA', $Wyglad->SrodekSklepu( 'gora', (( $GLOBALS['stronaGlowna'] == true ) ? array(1,2) : array(1,3,4) ) ));
 //!new!
-$tpl->dodaj('__MODULY_SRODKOWE_GORA', '');
+$tpl->dodaj('__MODULY_SRODKOWE_GORA', 'A');
 
 // moduly srodkowe pod czescia glowna sklepu z boxami  
 $tpl->dodaj('__MODULY_SRODKOWE_DOL', $Wyglad->SrodekSklepu( 'dol', (( $GLOBALS['stronaGlowna'] == true ) ? array(1,2) : array(1,3,4) ) ));
